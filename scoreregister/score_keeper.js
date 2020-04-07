2f87e3d7cd4d9760f6404e6efa6565e83de612f7
devashish singh <Devashishsingh98@github.com>
2020-04-07 20:13:28 +0530

    ADDED NEW FUNCTION.

--- scoreregister/score_keeper.js
+++ scoreregister/score_keeper.js
@@ -1,7 +1,10 @@
 var p1button=document.querySelector("#p1");
 var p2button=document.querySelector("#p2");
-var p1display= document.querySelector("#p1display")
-var p2display= document.querySelector("#p2display")
+var p1display= document.querySelector("#p1display");
+var p2display= document.querySelector("#p2display");
+var resetbutton=document.querySelector("#reset");
+var numInput=document.querySelector("input");
+var winningscoredisplay=document.querySelector("p span");
 var p1score=0, p2score=0;
 var gameover=false;
 var winningscore=5;
@@ -18,14 +21,39 @@ p1button.addEventListener("click",function(){
     }
     
 })
-
 p2button.addEventListener("click",function(){
     if(!gameover){
         p2score++;
         if(p2score===winningscore){
+            p2display.classList.add("winner");
             gameover=true;
         }
         p2display.textContent=p2score;
     }
 })
-
+resetbutton.addEventListener("click",function(){
+    reset();    
+})
+resetbutton.addEventListener("click",function(){
+    p1score=0;
+    p2score=0;
+    p1display.textContent=0;
+    p2display.textContent=0;
+    p1display.classList.remove("winner");
+    p2display.classList.remove("winner");
+    gameover=false;
+})
+function reset(){
+    p1score=0;
+    p2score=0;
+    p1display.textContent=0;
+    p2display.textContent=0;
+    p1display.classList.remove("winner");
+    p2display.classList.remove("winner");
+    gameover=false;
+}
+numInput.addEventListener("change",function(){
+    winningscoredisplay.textContent=numInput.value;
+    winningscore=Number(numInput.value);
+    reset();
+}) 
